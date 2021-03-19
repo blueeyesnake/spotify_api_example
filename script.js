a88cd29cbfb3783c1dbd02af7efc07611671b058
blueeyesnake <49039323+blueeyesnake@users.noreply.github.com>
2021-03-19 02:42:41 -0500

    change redirectories

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ $( document ).ready(function() {
 
     // AUTHORIZE with Spotify (if needed)
     // *************** REPLACE THESE VALUES! *************************
-    let client_id = 'df6968c34f4b4494ac206c7628d2afc7';
+    let client_id = '83e21f647e8a43b6ace1d38f77c7dd48';
     // Use the following site to convert your regular url to the encoded version:
     // https://www.url-encode-decode.com/
-    let redirect_uri = 'https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only'; // GitHub Pages URL or whatever your public url to this app is
+    let redirect_uri = 'https%3A%2F%2Fblueeyesnake.github.io%2Fspotify_api_example'; // GitHub Pages URL or whatever your public url to this app is
     // *************** END *************************
 
     const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
